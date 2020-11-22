a181e09aa3e9fcaba6db917925fb5ba1459c7994
Dimaskes <d.imon-million@yandex.ru>
2020-11-22 23:18:24 +0400

    выпадающее окно "добавить список"

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
+import Badge from '../Badge';
 
 import './List.scss';
 
-const List = ({items, isRemovable}) => {
-    console.log(isRemovable)
+const List = ({items, isRemovable, onClick}) => {
 
     return (
-        <ul className="list">
+        <ul onClick={onClick} className="list">
         {
             items.map((item, index) => (
                 <li key={index} className={classNames(item.className, {'active' : item.active})}>
-                    <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
+                    <i>{item.icon ? item.icon : <Badge color={item.color}/>}</i>
                     <span>{item.name}</span>
                 </li>
             ))
